9c7badbfe520e15c3b0b1a7f18142675a5b07956
Lorenzo Di Bernardo <lori.dibe107@gmail.com>
2026-04-22 20:41:45 +0200

    feat(ui): design tokens + globals

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './app/App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/tokens.css";
+import "./styles/globals.css";
+import App from "./app/App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
